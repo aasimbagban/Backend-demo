02e31761007e7ef993316ca8284720caa1294e2b
aasim bagban <mohmmadaasim378@gmail.com>
2026-05-18 22:05:26 +0530

    update userController

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,8 +53,8 @@ timestamps:true
 
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next();
-  this.password= bcrypt.hash(this.password,10)
-  next()
+  this.password= await bcrypt.hash(this.password,10)
+  // next()
 })
 
 userSchema.methods.isPasswordCorrect= async function (password) {
