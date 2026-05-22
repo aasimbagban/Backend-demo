326f927f7a6ce97be08a3146c2ee8e851fbcbcb1
aasim bagban <mohmmadaasim378@gmail.com>
2026-05-22 11:54:24 +0530

    login and logout middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,8 +16,10 @@ const uploadOnCloudinary=async (localFilePath)=>{
                 resource_type:"auto"
             }
         )
-         console.log("file is uploaded on cloudinary",response.url); 
+        //  console.log("file is uploaded on cloudinary",response.url); 
+         fs.unlinkSync(localFilePath)
          return response;
+
     } catch (error) {
         fs.unlinkSync(localFilePath)
         return null;
