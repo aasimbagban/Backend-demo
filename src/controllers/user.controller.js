aedd61bdb0ff5d8caf4416b14ce7dd3ad27170d7
aasim bagban <mohmmadaasim378@gmail.com>
2026-05-28 16:42:17 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,8 +146,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
@@ -237,7 +237,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully ");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully "));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
